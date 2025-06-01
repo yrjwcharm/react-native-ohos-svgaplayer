05761afaac1c098c1bfbe7bee99a4ec745e35ea0
yanruifeng <yanruifeng@syriusrobotics.com>
2025-06-01 18:38:08 +0800

    feat:优化

--- src/SvgaPlayerViewNativeComponent.ts
+++ src/SvgaPlayerViewNativeComponent.ts
@@ -1,9 +1,18 @@
 import {HostComponent, ViewProps} from 'react-native';
+import {
+  DirectEventHandler,
+  Float,
+} from 'react-native/Libraries/Types/CodegenTypes';
 import codegenNativeCommands from 'react-native/Libraries/Utilities/codegenNativeCommands';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-
+interface ICallbacks {
+  value: Float;
+}
 interface SvgaPlayerProps extends ViewProps {
   source: string;
+  onFinished?: DirectEventHandler<{}>;
+  onFrame?: DirectEventHandler<ICallbacks>;
+  onPercentage?: DirectEventHandler<ICallbacks>;
 }
 type NativeType = HostComponent<SvgaPlayerProps>;
 interface NativeCommands {
